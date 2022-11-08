31d25d3787badcf15aa7578056305f69cc6bb107
katrina Young <hello@katrinayoungconsulting.com>
2022-11-08 20:49:09 +0000

    Task 1 React session 3

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const name = '[YOUR NAME GOES HERE]';
+const name = 'Name';
 
 function Profile(name){
+  if (name.length> 0 ){
     return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
               <p>Example of react JSX in action</p>
             </main>
-}
+            }else {
+              return <main id="content" role="main" className="base">
+                        <h1> Just a freaking React Page</h1>
+                          <p>Example of react JSX in action</p>
+                        </main>
+            }
+} 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Profile());
+root.render(Profile(name));
 
